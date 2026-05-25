01f13cf79173ced7387e010687a08a2905db61d1
zainab-khan059 <zainabsajid2601@gmail.com>
2026-05-25 00:24:57 -0700

    Added DATABASE FOLDER and renamed README.md  file.

---
+++ Database/AppData.cs
@@ -0,0 +1,32 @@
+﻿namespace ReadLog.Database
+{
+    public static class AppData
+    {
+        public static string[] Genres =
+        {
+            "Sci-Fi",
+            "Fantasy",
+            "Self Help",
+            "Programming",
+            "Novel",
+            "Biography",
+            "History"
+        };
+
+        public static string[] Status =
+        {
+            "Read",
+            "Currently Reading",
+            "Want To Read"
+        };
+
+        public static string[] Ratings =
+        {
+            "1 Star",
+            "2 Stars",
+            "3 Stars",
+            "4 Stars",
+            "5 Stars"
+        };
+    }
+}
---
+++ Database/DatabaseHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ReadLog.Database
+{
+    public static class DatabaseHelper
+    {
+        // Change the text after AttachDbFilename= to match the real path on your computer!
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\ReadLog\Database\ReadLogDB.mdf;Integrated Security=True;";
+        // SELECT / READ DATA METHOD
+        public static DataTable ExecuteQuery(string query, SqlParameter[] parameters = null)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        conn.Open();
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
+        // INSERT / UPDATE / DELETE METHOD
+        public static int ExecuteNonQuery(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
